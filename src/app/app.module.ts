3b7c173397da0cc7409f85fe5c94e98e450ea65e
JulieNonnon <julie.nonnon@gmail.com>
2024-02-04 22:52:13 +0100

    Feature: implementation of header, navigation and features to be developed later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestGlobalCSSComponent } from './components/test-global-css/test-global-css.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestGlobalCSSComponent
+    TestGlobalCSSComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
