63a07cb4d990e1f4080d0a7a07e022fce1655a5c
JulieNonnon <julie.nonnon@gmail.com>
2024-02-05 01:08:08 +0100

    feature: router implemented, page home and page error404 created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'error404', component: Error404Component },
+  { path: '**', redirectTo: '/error404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
