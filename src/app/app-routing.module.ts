db70f82d3282f6ca6817aa794446fbfd07d74a21
JulieNonnon <julie.nonnon@gmail.com>
2024-02-07 20:36:23 +0100

    feature: v1 pages created, first navigation links and banners implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { Error404Component } from './pages/error404/error404.component';
+import { AllCategoriesComponent } from './pages/all-categories/all-categories.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { OrderSummaryComponent } from './pages/order-summary/order-summary.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'all-categories', component: AllCategoriesComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'order-summary', component: OrderSummaryComponent },
   { path: 'error404', component: Error404Component },
   { path: '**', redirectTo: '/error404' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ButtonComponent } from './reusables/button/button.component';
-import { BannerComponent } from './reusables/banner/banner.component'
+import { BannerComponent } from './reusables/banner/banner.component';
+import { AllCategoriesComponent } from './pages/all-categories/all-categories.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { OrderSummaryComponent } from './pages/order-summary/order-summary.component'
 
 @NgModule({
   declarations: [
@@ -23,6 +28,11 @@ import { BannerComponent } from './reusables/banner/banner.component'
     FooterComponent,
     ButtonComponent,
     BannerComponent,
+    AllCategoriesComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrderSummaryComponent,
   ],
   imports: [
     BrowserModule,
