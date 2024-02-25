662e57491819dee58a83b031c2904742ec44499b
JulieNonnon <julie.nonnon@gmail.com>
2024-02-25 16:06:07 +0100

    feature: legal-cgv page created, link implemented in footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProductDetailsComponent } from './pages/product-details/product-details
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { OrderSummaryComponent } from './pages/order-summary/order-summary.component';
+import { LegalCgvComponent } from './pages/legal-cgv/legal-cgv.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'order-summary', component: OrderSummaryComponent },
+  { path: 'legal-cgv', component: LegalCgvComponent },
   { path: 'error404', component: Error404Component },
   { path: '**', redirectTo: '/error404' }
 ];
---
+++ src/app/pages/legal-cgv/legal-cgv.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-cgv',
+  templateUrl: './legal-cgv.component.html',
+  styleUrl: './legal-cgv.component.css'
+})
+export class LegalCgvComponent {
+
+}
