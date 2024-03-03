728b9a80d170399493d75cd322a1f56071babfc4
JulieNonnon <julie.nonnon@gmail.com>
2024-03-03 16:14:07 +0100

    feature: product-details-card created, implemented in product-details page, dynamic navigation to be implemented

---
+++ src/app/reusables/product-details-card/product-details-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-card',
+  templateUrl: './product-details-card.component.html',
+  styleUrl: './product-details-card.component.css'
+})
+export class ProductDetailsCardComponent {
+
+}
