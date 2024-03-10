48b8a1d0ab16a5b5d1d7a8c89b7360cf44a3417a
JulieNonnon <julie.nonnon@gmail.com>
2024-03-10 23:20:24 +0100

    feature: implementation of dynamic product nav with mock

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../mocks/product.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,12 @@ export class ProductService {
       img: 'assets/img/product-coeur-de-boeuf.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "Découvrez l'exquise Tomate Cœur de Bœuf, une variété qui séduira les amateurs de tomates par sa forme distinctive et son goût exceptionnel. Ces tomates sont caractérisées par leur grande taille, leur forme côtelée et leur chair charnue, offrant une expérience gustative riche et savoureuse.", 
-      price: 4 
+      price: 4,
+      pitch: "Faites de la Tomate Cœur de Bœuf le joyau de votre jardin et savourez des récoltes abondantes de ces délicieuses tomates. Transformez vos plats en créations culinaires irrésistibles grâce à cette variété exceptionnelle.",
+      feature1: "Forme Unique : Les tomates Cœur de Bœuf se distinguent par leur forme généreuse et arrondie, évoquant celle d'un cœur. Chair Charpentée : La chair de ces tomates est juteuse, dense et pleine de saveurs, idéale pour des salades gourmandes, des sauces ou des sandwiches.",
+      feature2: "Sucrosité Délicate : Les tomates Cœur de Bœuf offrent une douceur naturelle équilibrée avec une légère acidité, créant une combinaison de saveurs inoubliable. Arôme Enivrant : Chaque bouchée libère un arôme frais et enivrant qui capture l'essence même des tomates cultivées avec soin.",
+      feature3: "Semences de Qualité : Nos graines de tomates Cœur de Bœuf sont sélectionnées avec soin pour garantir des plants vigoureux et productifs. Cultivation Responsable : Nous nous engageons à promouvoir des pratiques agricoles durables pour vous offrir des produits de la plus haute qualité.",
+      category: "classic", 
     },
     { 
       id: 2, 
@@ -21,7 +27,12 @@ export class ProductService {
       img: 'assets/img/product-beauty-queen.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "BEAUTY QUEEN PLACEHOLDER", 
-      price: 5 
+      price: 5,
+      pitch: "",
+      feature1: "feature",
+      feature2: "feature",
+      feature3: "feature",
+      category: "classic",  
     },
     { 
       id: 3, 
@@ -29,7 +40,12 @@ export class ProductService {
       img: 'assets/img/product-noire-de-crimee.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "NOIRE DE CRIMEE PLACEHOLDER", 
-      price: 3 
+      price: 3,
+      pitch: "",
+      feature1: "feature",
+      feature2: "feature",
+      feature3: "feature",
+      category: "classic",  
     },
     { 
       id: 4, 
@@ -37,7 +53,12 @@ export class ProductService {
       img: 'assets/img/product-ananas.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "ANANAS PLACEHOLDER", 
-      price: 6 
+      price: 6,
+      pitch: "",
+      feature1: "feature",
+      feature2: "feature",
+      feature3: "feature",
+      category: "classic",  
     },
     { 
       id: 5, 
@@ -45,7 +66,12 @@ export class ProductService {
       img: 'assets/img/product-green-zebra.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "GREEN ZEBRA PLACEHOLDER", 
-      price: 2 
+      price: 2,
+      pitch: "",
+      feature1: "feature",
+      feature2: "feature",
+      feature3: "feature",
+      category: "classic",  
     },
     { 
       id: 6, 
@@ -53,15 +79,32 @@ export class ProductService {
       img: 'assets/img/product-white-beauty.png',
       smallDescription: "Sachet individuel d’une quinzaine de graines.", 
       longDescription: "WHITE BEAUTY PLACEHOLDER", 
-      price: 66.55 
+      price: 66.55,
+      pitch: "",
+      feature1: "feature",
+      feature2: "feature",
+      feature3: "feature",
+      category: "classic",  
     },
-    // Ajoutez les autres produits de manière similaire
   ];
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   getProducts(): Product[] {
     return this.products;
   }
 
+  //Méthode pour récupération d'un produit par son Id
+  //Condition: produit retourné si son id existe
+  getProductById(id: number): Product {
+    const product = this.products.find(product => product.id === id);
+    if (product) {
+      return product;
+    } else {
+      this.router.navigateByUrl('/error404');
+      throw new Error("le produit avec l'id ${id} est introuvable.");
+    }
+  }
 }
