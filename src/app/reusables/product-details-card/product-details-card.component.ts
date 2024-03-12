563f2e7d72770924e4b1a042e17915e1564426aa
JulieNonnon <julie.nonnon@gmail.com>
2024-03-12 22:40:35 +0100

    feature: counter added, to be improved

--- src/app/reusables/product-details-card/product-details-card.component.ts
+++ src/app/reusables/product-details-card/product-details-card.component.ts
@@ -7,7 +7,21 @@ import { Product } from '../../mocks/product.model';
   styleUrl: './product-details-card.component.css'
 })
 export class ProductDetailsCardComponent {
+  
   @Input() product!: Product;
+
+  productQuantity : number = 1;
+  totalPrice: number = 0;
+  
+  updateQuantity(quantity: number) {
+    this.productQuantity = quantity;
+    this.calculateTotalPrice();
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = this.product.price * this.productQuantity;
+  }
+
 
 
 }
